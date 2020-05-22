dc79ced04b6469bba9defec4b54f44d79aa4b4fc
Vladimir Popov <papoff8295@gmail.com>
2020-05-22 12:38:52 +0300

    reload5

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -131,7 +131,7 @@ public class Bot extends TelegramLongPollingBot {
         rowList.add(keyboardButtonsRow1);
         rowList.add(keyboardButtonsRow2);
         inlineKeyboardMarkup.setKeyboard(rowList);
-        String text = "Здравствуйте " + update.getMessage().getContact().getFirstName();
+        String text = "Здравствуйте ";
         return new SendMessage().setChatId(chatId).setText(text).setReplyMarkup(inlineKeyboardMarkup);
     }
 
